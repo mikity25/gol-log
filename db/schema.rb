550c17bbe17ded424f93c1e88a08292ef1b272a4
MIKITY25 <mtmk25@gmail.com>
2026-08-27 14:00:31 +0900

    add: Recordモデルの生成・データベースへ反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_26_143405) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_27_045048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "records", force: :cascade do |t|
+    t.integer "converted_score_18h"
+    t.datetime "created_at", null: false
+    t.integer "difficulty"
+    t.integer "facility"
+    t.integer "food"
+    t.string "golf_course_name", null: false
+    t.text "memo"
+    t.date "played_on", null: false
+    t.integer "satisfaction", null: false
+    t.integer "score_18h"
+    t.integer "score_9h"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,4 +43,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_26_143405) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "records", "users"
 end
